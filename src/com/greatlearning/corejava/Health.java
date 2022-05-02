ad5f951c16021576fcaa05313b19df60af447838
HammadSala <flash_hammad@yahoo.co.in>
2022-05-02 15:24:02 +0530

    deleteing
    deleteing

--- src/com/greatlearning/corejava/Health.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.corejava;
-
-public class Health {
-
-		protected int age =33;
-		protected float weight = 78.5f;
-		protected float bmi = 33;
-		
-}
--- src/com/greatlearning/corejava/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Hammad-PC
- *
- */
-package com.greatlearning.corejava;
